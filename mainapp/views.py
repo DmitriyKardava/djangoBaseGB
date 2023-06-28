dbba0ced7de26fa08733d7af7f743c18bf48b948
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-06-28 13:42:27 +0300

    lesson3 templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,29 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.shortcuts import redirect
+from requests.models import PreparedRequest
 
+import json
+from django.conf import settings
+import os
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
     
-class NewsPageView(TemplateView):
+class NewsPageView(ListView):
     template_name = "mainapp/news.html"
+    paginate_by = 5
+
+    def get_queryset(self):
+        with open(os.path.join(settings.BASE_DIR, 'data/news.json')) as news_file:
+            return json.load(news_file)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["news_title"] = "Тестовая новость для поиска"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["search_str"] = "описание"
+        context["search_str2"] = "заинтересует"
+        return context            
 
 class CoursesPageView(TemplateView):
     template_name = "mainapp/courses_list.html"
@@ -18,3 +36,11 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
+
+def SearchRedirect(request, search_str=''):
+    req = PreparedRequest()
+    url = "https://google.com/search"
+    params = {'q':search_str}
+    req.prepare_url(url, params)
+    respose = redirect(req.url)
+    return respose
