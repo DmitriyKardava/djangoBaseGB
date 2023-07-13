7029d2312b467ddc40e1e5c7ca053bf14ac8890a
Dmitriy Kardava <dmitriy.kardava@gmail.com>
2023-07-13 19:38:58 +0300

    added edit profile

--- authapp/views.py
+++ authapp/views.py
@@ -71,4 +71,31 @@ class RegisterView(TemplateView):
         
 
 class ProfileEditView(LoginRequiredMixin, TemplateView):
-    pass
+    template_name = "registration/profile_edit.html" 
+    login_url = reverse_lazy("authapp:login")
+    
+    def post(self, request, *args, **kwargs):
+        try:
+            if request.POST.get("username"):
+                request.user.username = request.POST.get("username") 
+            if request.POST.get("first_name"):
+                request.user.first_name = request.POST.get("first_name") 
+            if request.POST.get("last_name"):
+                request.user.last_name = request.POST.get("last_name") 
+            if request.POST.get("age"):
+                request.user.age = request.POST.get("age") 
+            if request.POST.get("email"):
+                request.user.email = request.POST.get("email") 
+            if request.FILES.get("avatar"):
+                if request.user.avatar and os.path.exists( request.user.avatar.path):
+                    os.remove(request.user.avatar.path)
+            request.user.avatar = request.FILES.get("avatar") 
+            request.user.save()
+            messages.add_message(request, messages.INFO, _("Saved!"))
+        except Exception as exp: messages.add_message(
+            request,
+            messages.WARNING,
+            mark_safe(f"Something goes worng:<br>{exp}"),
+            )
+        return HttpResponseRedirect(reverse_lazy("authapp:profile_edit"))
+    
